ef6eda8a8bacec8f914ebbb4aa49ffa5bb677256
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2021-12-16 16:56:49 +0300

    WIP

--- 50214/bson/array_test.go
+++ 50214/bson/array_test.go
@@ -16,7 +16,6 @@ package bson
 
 import (
 	"testing"
-	"time"
 
 	"github.com/AlekSi/go-bug/50214/types"
 	"github.com/AlekSi/go-bug/50214/util/testutil"
@@ -28,20 +27,12 @@ var arrayTestCases = []testCase{{
 		types.Array{},
 		types.Binary{Subtype: types.BinaryUser, B: []byte{0x42}},
 		true,
-		time.Date(2021, 7, 27, 9, 35, 42, 123000000, time.UTC).Local(),
 		types.MustMakeDocument(),
 		42.13,
-		int32(42),
-		int64(42),
 		"foo",
-		nil,
 	},
 	b: testutil.MustParseDumpFile("testdata", "array_all.hex"),
-	j: `[[],{"$b":"Qg==","s":128},true,{"$d":1627378542123},{"$k":[]},{"$f":42.13},42,{"$l":"42"},"foo",null]`,
-}, {
-	name: "array_fuzz1",
-	b:    testutil.MustParseDumpFile("testdata", "array_fuzz1.hex"),
-	bErr: `key 0 is "8"`,
+	j: "[[],{\"$b\":\"Qg==\",\"s\":128},true,{\"$k\":[]},{\"$f\":42.13},\"foo\"]",
 }}
 
 func TestArray(t *testing.T) {
--- 50214/bson/document_test.go
+++ 50214/bson/document_test.go
@@ -16,7 +16,6 @@ package bson
 
 import (
 	"testing"
-	"time"
 
 	"github.com/AlekSi/go-bug/50214/types"
 	"github.com/AlekSi/go-bug/50214/util/testutil"
@@ -90,7 +89,6 @@ var (
 	handshake3 = testCase{
 		name: "handshake3",
 		v: MustConvertDocument(types.MustMakeDocument(
-			"buildInfo", int32(1),
 			"lsid", types.MustMakeDocument(
 				"id", types.Binary{
 					Subtype: types.BinaryUUID,
@@ -100,106 +98,10 @@ var (
 			"$db", "admin",
 		)),
 		b: testutil.MustParseDumpFile("testdata", "handshake3.hex"),
-		j: `{"$k":["buildInfo","lsid","$db"],"buildInfo":1,` +
-			`"lsid":{"$k":["id"],"id":{"$b":"oxnytKF1QMe456OjLsJWvg==","s":4}},"$db":"admin"}`,
+		j: "{\"$k\":[\"lsid\",\"$db\"],\"lsid\":{\"$k\":[\"id\"],\"id\":{\"$b\":\"oxnytKF1QMe456OjLsJWvg==\",\"s\":4}},\"$db\":\"admin\"}",
 	}
 
-	handshake4 = testCase{
-		name: "handshake4",
-		v: MustConvertDocument(types.MustMakeDocument(
-			"version", "5.0.0",
-			"gitVersion", "1184f004a99660de6f5e745573419bda8a28c0e9",
-			"modules", types.Array{},
-			"allocator", "tcmalloc",
-			"javascriptEngine", "mozjs",
-			"sysInfo", "deprecated",
-			"versionArray", types.Array{int32(5), int32(0), int32(0), int32(0)},
-			"openssl", types.MustMakeDocument(
-				"running", "OpenSSL 1.1.1f  31 Mar 2020",
-				"compiled", "OpenSSL 1.1.1f  31 Mar 2020",
-			),
-			"buildEnvironment", types.MustMakeDocument(
-				"distmod", "ubuntu2004",
-				"distarch", "x86_64",
-				"cc", "/opt/mongodbtoolchain/v3/bin/gcc: gcc (GCC) 8.5.0",
-				"ccflags", "-Werror -include mongo/platform/basic.h -fasynchronous-unwind-tables -ggdb "+
-					"-Wall -Wsign-compare -Wno-unknown-pragmas -Winvalid-pch -fno-omit-frame-pointer "+
-					"-fno-strict-aliasing -O2 -march=sandybridge -mtune=generic -mprefer-vector-width=128 "+
-					"-Wno-unused-local-typedefs -Wno-unused-function -Wno-deprecated-declarations "+
-					"-Wno-unused-const-variable -Wno-unused-but-set-variable -Wno-missing-braces "+
-					"-fstack-protector-strong -Wa,--nocompress-debug-sections -fno-builtin-memcmp",
-				"cxx", "/opt/mongodbtoolchain/v3/bin/g++: g++ (GCC) 8.5.0",
-				"cxxflags", "-Woverloaded-virtual -Wno-maybe-uninitialized -fsized-deallocation -std=c++17",
-				"linkflags", "-Wl,--fatal-warnings -pthread -Wl,-z,now -fuse-ld=gold -fstack-protector-strong "+
-					"-Wl,--no-threads -Wl,--build-id -Wl,--hash-style=gnu -Wl,-z,noexecstack -Wl,--warn-execstack "+
-					"-Wl,-z,relro -Wl,--compress-debug-sections=none -Wl,-z,origin -Wl,--enable-new-dtags",
-				"target_arch", "x86_64",
-				"target_os", "linux",
-				"cppdefines", "SAFEINT_USE_INTRINSICS 0 PCRE_STATIC NDEBUG _XOPEN_SOURCE 700 _GNU_SOURCE "+
-					"_REENTRANT 1 _FORTIFY_SOURCE 2 BOOST_THREAD_VERSION 5 BOOST_THREAD_USES_DATETIME "+
-					"BOOST_SYSTEM_NO_DEPRECATED BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS "+
-					"BOOST_ENABLE_ASSERT_DEBUG_HANDLER BOOST_LOG_NO_SHORTHAND_NAMES BOOST_LOG_USE_NATIVE_SYSLOG "+
-					"BOOST_LOG_WITHOUT_THREAD_ATTR ABSL_FORCE_ALIGNED_ACCESS",
-			),
-			"bits", int32(64),
-			"debug", false,
-			"maxBsonObjectSize", int32(16777216),
-			"storageEngines", types.Array{"devnull", "ephemeralForTest", "wiredTiger"},
-			"ok", float64(1),
-		)),
-		b: testutil.MustParseDumpFile("testdata", "handshake4.hex"),
-		j: `{"$k":["version","gitVersion","modules","allocator","javascriptEngine","sysInfo","versionArray",` +
-			`"openssl","buildEnvironment","bits","debug","maxBsonObjectSize","storageEngines","ok"],` +
-			`"version":"5.0.0","gitVersion":"1184f004a99660de6f5e745573419bda8a28c0e9","modules":[],` +
-			`"allocator":"tcmalloc","javascriptEngine":"mozjs","sysInfo":"deprecated","versionArray":[5,0,0,0],` +
-			`"openssl":{"$k":["running","compiled"],"running":"OpenSSL 1.1.1f  31 Mar 2020",` +
-			`"compiled":"OpenSSL 1.1.1f  31 Mar 2020"},` +
-			`"buildEnvironment":{"$k":["distmod","distarch","cc","ccflags","cxx","cxxflags","linkflags",` +
-			`"target_arch","target_os","cppdefines"],"distmod":"ubuntu2004","distarch":"x86_64",` +
-			`"cc":"/opt/mongodbtoolchain/v3/bin/gcc: gcc (GCC) 8.5.0",` +
-			`"ccflags":"-Werror -include mongo/platform/basic.h -fasynchronous-unwind-tables -ggdb -Wall ` +
-			`-Wsign-compare -Wno-unknown-pragmas -Winvalid-pch -fno-omit-frame-pointer -fno-strict-aliasing ` +
-			`-O2 -march=sandybridge -mtune=generic -mprefer-vector-width=128 -Wno-unused-local-typedefs ` +
-			`-Wno-unused-function -Wno-deprecated-declarations -Wno-unused-const-variable ` +
-			`-Wno-unused-but-set-variable -Wno-missing-braces -fstack-protector-strong ` +
-			`-Wa,--nocompress-debug-sections -fno-builtin-memcmp",` +
-			`"cxx":"/opt/mongodbtoolchain/v3/bin/g++: g++ (GCC) 8.5.0",` +
-			`"cxxflags":"-Woverloaded-virtual -Wno-maybe-uninitialized -fsized-deallocation -std=c++17",` +
-			`"linkflags":"-Wl,--fatal-warnings -pthread -Wl,-z,now -fuse-ld=gold -fstack-protector-strong ` +
-			`-Wl,--no-threads -Wl,--build-id -Wl,--hash-style=gnu -Wl,-z,noexecstack -Wl,--warn-execstack ` +
-			`-Wl,-z,relro -Wl,--compress-debug-sections=none -Wl,-z,origin -Wl,--enable-new-dtags",` +
-			`"target_arch":"x86_64","target_os":"linux",` +
-			`"cppdefines":"SAFEINT_USE_INTRINSICS 0 PCRE_STATIC NDEBUG _XOPEN_SOURCE 700 _GNU_SOURCE ` +
-			`_REENTRANT 1 _FORTIFY_SOURCE 2 BOOST_THREAD_VERSION 5 BOOST_THREAD_USES_DATETIME ` +
-			`BOOST_SYSTEM_NO_DEPRECATED BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS BOOST_ENABLE_ASSERT_DEBUG_HANDLER ` +
-			`BOOST_LOG_NO_SHORTHAND_NAMES BOOST_LOG_USE_NATIVE_SYSLOG BOOST_LOG_WITHOUT_THREAD_ATTR ` +
-			`ABSL_FORCE_ALIGNED_ACCESS"},"bits":64,"debug":false,"maxBsonObjectSize":16777216,` +
-			`"storageEngines":["devnull","ephemeralForTest","wiredTiger"],"ok":{"$f":1}}`,
-	}
-
-	all = testCase{
-		name: "all",
-		v: MustConvertDocument(types.MustMakeDocument(
-			"binary", types.Array{
-				types.Binary{Subtype: types.BinaryUser, B: []byte{0x42}},
-				types.Binary{Subtype: types.BinaryGeneric, B: []byte{}},
-			},
-			"bool", types.Array{true, false},
-			"datetime", types.Array{time.Date(2021, 7, 27, 9, 35, 42, 123000000, time.UTC).Local(), time.Time{}.Local()},
-			"double", types.Array{42.13, 0.0},
-			"int32", types.Array{int32(42), int32(0)},
-			"int64", types.Array{int64(42), int64(0)},
-			"string", types.Array{"foo", ""},
-		)),
-		b: testutil.MustParseDumpFile("testdata", "all.hex"),
-		j: `{"$k":["binary","bool","datetime","double","int32","int64","string"],` +
-			`"binary":[{"$b":"Qg==","s":128},{"$b":"","s":0}],"bool":[true,false],` +
-			`"datetime":[{"$d":1627378542123},{"$d":-62135596800000}],"double":[{"$f":42.13},{"$f":0}],` +
-			`"int32":[42,0],"int64":[{"$l":"42"},{"$l":"0"}],` +
-			`"string":["foo",""]`,
-	}
-
-	documentTestCases = []testCase{handshake1, handshake2, handshake3, handshake4, all}
+	documentTestCases = []testCase{handshake1, handshake2, handshake3}
 )
 
 func TestDocument(t *testing.T) {
