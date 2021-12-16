a9b75eb977fa49c576f4de2cb9b871fcf7dae8f1
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2021-12-16 17:54:21 +0300

    WIP

--- 50214/bug_test.go
+++ 50214/bug_test.go
@@ -2,8 +2,6 @@ package bug
 
 import (
 	"bufio"
-	"bytes"
-	"reflect"
 	"testing"
 )
 
@@ -11,11 +9,6 @@ type Object struct{}
 
 func (o *Object) ReadFrom(r *bufio.Reader) error {
 	_, err := r.ReadByte()
-	return err
-}
-
-func (o Object) WriteTo(w *bufio.Writer) error {
-	_, err := w.Write([]byte{0x42})
 	return err
 }
 
@@ -35,50 +28,6 @@ func fuzzBinary(f *testing.F, testCases []testCase) {
 
 	f.Fuzz(func(t *testing.T, b []byte) {
 		t.Parallel()
-
-		var v *Object
-		var expectedB []byte
-
-		// test ReadFrom
-		{
-			v = new(Object)
-			br := bytes.NewReader(b)
-			bufr := bufio.NewReader(br)
-			if err := v.ReadFrom(bufr); err != nil {
-				t.Skip(err)
-			}
-
-			// remove random tail
-			expectedB = b[:len(b)-bufr.Buffered()-br.Len()]
-		}
-
-		// test MarshalBinary
-		{
-			actualB, err := v.MarshalBinary()
-			if err != nil {
-				t.Fatal(err)
-			}
-			if !reflect.DeepEqual(expectedB, actualB) {
-				t.Fatal("MarshalBinary results differ")
-			}
-		}
-
-		// test WriteTo
-		{
-			var bw bytes.Buffer
-			bufw := bufio.NewWriter(&bw)
-			err := v.WriteTo(bufw)
-			if err != nil {
-				t.Fatal(err)
-			}
-			err = bufw.Flush()
-			if err != nil {
-				t.Fatal(err)
-			}
-			if !reflect.DeepEqual(expectedB, bw.Bytes()) {
-				t.Fatal("WriteTo results differ")
-			}
-		}
 	})
 }
 
