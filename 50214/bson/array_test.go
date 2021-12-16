de9711e97606bb219189b875da3f0b33bc6cef92
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2021-12-16 17:19:45 +0300

    WIP

--- 50214/bson/array_test.go
+++ 50214/bson/array_test.go
@@ -1,11 +1,63 @@
 package bson
 
 import (
+	"bufio"
+	"encoding/hex"
+	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/AlekSi/go-bug/50214/types"
-	"github.com/AlekSi/go-bug/50214/util/testutil"
 )
+
+func parseDump(s string) ([]byte, error) {
+	var res []byte
+
+	scanner := bufio.NewScanner(strings.NewReader(strings.TrimSpace(s)))
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" {
+			continue
+		}
+
+		if line[len(line)-1] == '|' {
+			// go dump
+			line = strings.TrimSpace(line[8:60])
+			line = strings.Join(strings.Split(line, " "), "")
+		} else {
+			// wireshark dump
+			line = strings.TrimSpace(line[7:54])
+			line = strings.Join(strings.Split(line, " "), "")
+		}
+
+		b, err := hex.DecodeString(line)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, b...)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func mustParseDumpFile(path ...string) []byte {
+	b, err := os.ReadFile(filepath.Join(path...))
+	if err != nil {
+		panic(err)
+	}
+
+	b, err = parseDump(string(b))
+	if err != nil {
+		panic(err)
+	}
+
+	return b
+}
 
 var arrayTestCases = []testCase{{
 	name: "array_all",
@@ -13,7 +65,7 @@ var arrayTestCases = []testCase{{
 		types.Array{},
 		types.MustMakeDocument(),
 	},
-	b: testutil.MustParseDumpFile("testdata", "array_all.hex"),
+	b: mustParseDumpFile("testdata", "array_all.hex"),
 	j: "[[],{\"$k\":[]}]",
 }}
 
