4305a63214b31ad213f8c05ac6296fddd2112118
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2024-06-20 14:20:53 +0400

    Update code

--- getwd/getwd.go
+++ getwd/getwd.go
@@ -1,13 +1,25 @@
-//go:build ignore
-
 package main
 
 import (
 	"fmt"
 	"os"
+
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/singlechecker"
 )
 
+var analyzer = &analysis.Analyzer{
+	Name: "getwd",
+	Doc:  "getwd",
+	URL:  "TODO",
+	Run: func(pass *analysis.Pass) (any, error) {
+		wd, _ := os.Getwd()
+		fmt.Printf("os.Getwd returned %s\n", wd)
+		fmt.Printf("os.Getwd returned %q\n", wd)
+		return nil, nil
+	},
+}
+
 func main() {
-	wd, err := os.Getwd()
-	fmt.Printf("main: wd = %q, err = %v\n", wd, err)
+	singlechecker.Main(analyzer)
 }
